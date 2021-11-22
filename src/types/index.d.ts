fc081ed254787dccc5ab6d1d06f2eb5917c0c7e1
Jose Mendoza <josemendozamatheus@gmail.com>
2021-11-22 00:37:45 -0500

    Local asset loading changed to be module imports
    Asset importing will not be done through the creation of a new URL temporary object, instead, it will be looked at through what asset is being imported.

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -7,3 +7,13 @@ declare module '*.frag' {
   const content: string;
   export default content;
 }
+
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.obj' {
+  const content: any;
+  export default content;
+}
